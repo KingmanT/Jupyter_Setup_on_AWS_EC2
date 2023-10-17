166b291ef27afe066b29909152cce750a918d565
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-10-17 10:52:12 -0400

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add your user to the 'docker' group
+sudo usermod -aG docker $USER
+
+# Apply changes to group
+newgrp docker
+
+# Verify that Docker was installed and running properly
+docker run hello-world
